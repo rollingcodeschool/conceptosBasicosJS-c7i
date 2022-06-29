89c6ba99037aec15101b5eeddee2da2f07181137
Emilse Arias <emilsearias@gmail.com>
2022-06-29 11:39:43 -0300

    comenzamos a crear variables

--- js/1-variables.js
+++ js/1-variables.js
@@ -8,13 +8,24 @@ lineas */
 document.write("<h1>Hola mundo</h1>");
 
 // mostrar un mensaje en la consola
-console.log('Nuevo mensaje');
+console.log("Nuevo mensaje");
 
 // mostrar un mensaje en una ventana emergente
 // alert('prueba de mensaje en ventana emergente');
 
 // mostrar una suma 29 + 6
-document.write('Resultado: ' + (29 + 6));
+document.write("Resultado: " + (29 + 6));
 // Resultado: 35
+
+// crear o declarar o inicializar una variable ( var - let - const)
+
+let persona = "Silvia Aymat";
+let comision = 7;
+
+// document.write("<p>Nombre: " + persona + "</p>");
+document.write("<p>Nombre: " + persona + "</p>");
+
+
+
 
 
