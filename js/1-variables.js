0a29cdac7b6f9d54b65d27893f8aac475b831cb3
Emilse Arias <emilsearias@gmail.com>
2022-06-29 11:14:38 -0300

    agreamos el codigo para mostrar diferentes mensajes

---
+++ js/1-variables.js
@@ -0,0 +1,20 @@
+//Esto es un comentario de una linea
+
+/* Esto es un comentario
+de multiples 
+lineas */
+
+// mostrar un mensaje en el documento html
+document.write("<h1>Hola mundo</h1>");
+
+// mostrar un mensaje en la consola
+console.log('Nuevo mensaje');
+
+// mostrar un mensaje en una ventana emergente
+// alert('prueba de mensaje en ventana emergente');
+
+// mostrar una suma 29 + 6
+document.write('Resultado: ' + (29 + 6));
+// Resultado: 35
+
+
