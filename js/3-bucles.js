eaf2adfb8f8ebfcb341e7d3866bcf1ab0be2a3d2
Emilse Arias <emilsearias@gmail.com>
2022-07-06 11:32:55 -0300

    se agrego el codigo de bucles while

---
+++ js/3-bucles.js
@@ -0,0 +1,14 @@
+/*
+while(codicion logica){
+    todo el codigo que quiero que se repita
+    cambiar la condicion logica
+}
+
+*/
+
+let cantidadMates = 20;
+
+while(cantidadMates >= 1){
+    document.write('<br>Mates cebados: '+cantidadMates);
+    cantidadMates--; //cantidadMates = cantidadMates - 1;
+}
