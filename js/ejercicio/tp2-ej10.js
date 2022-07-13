553cfdf55ce2d08a3d499559d21eeaa427a56a64
Emilse Arias <emilsearias@gmail.com>
2022-07-13 11:22:50 -0300

    agregamos el valor correspondiente a cada celda

--- js/ejercicio/tp2-ej10.js
+++ js/ejercicio/tp2-ej10.js
@@ -3,8 +3,9 @@
 //Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente ej 7X5 = de 35 al 1
 
 
-let filas = parseInt(prompt('Ingrese el numero de filas'));
-let columnas = parseInt(prompt('Ingrese un numero de columnas'));
+const filas = parseInt(prompt('Ingrese el numero de filas'));
+const columnas = parseInt(prompt('Ingrese un numero de columnas'));
+let resultado = filas * columnas;
 
 console.log(filas);
 console.log(columnas);
@@ -15,7 +16,8 @@ console.log(columnas);
     for(let inidiceFila = 0; inidiceFila < filas; inidiceFila++ ){
         document.write(`<tr>`)
         for(let indiceColumna = 0; indiceColumna < columnas; indiceColumna++){
-            document.write(`<td>hola</td>`)
+            document.write(`<td>${resultado}</td>`);
+            resultado--; //resultado = resultado - 1
         }
         document.write(`</tr>`)
     }
