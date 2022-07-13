396adbdf50847544d1cbbeec523d19add577745c
Emilse Arias <emilsearias@gmail.com>
2022-07-13 10:53:19 -0300

    el codigo sabe dibujar las filas que indica el usuario

---
+++ js/ejercicio/tp2-ej10.js
@@ -0,0 +1,26 @@
+//pedir al usuario un numero de filas y columnas X
+//dibujar una tabla X
+//Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente ej 7X5 = de 35 al 1
+
+
+let filas = parseInt(prompt('Ingrese el numero de filas'));
+let columnas = parseInt(prompt('Ingrese un numero de columnas'));
+
+console.log(filas);
+console.log(columnas);
+
+    document.write(`<table>
+    <tbody>`)
+
+    for(let inidiceFila = 0; inidiceFila < filas; inidiceFila++ ){
+        document.write(`
+            <tr>
+                <td>hola</td>
+                <td>mundo 2</td>
+            </tr>`)
+    }
+
+
+    document.write(`
+    </tbody>
+    </table>`)
