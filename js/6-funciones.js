fad2d1d66795bce3012cbcbc1796569d239f3a22
Emilse Arias <emilsearias@gmail.com>
2022-07-18 11:35:56 -0300

    creamos una funcion sin parametros

---
+++ js/6-funciones.js
@@ -0,0 +1,8 @@
+//funcion sin parametros
+function saludar(){
+    //todo el codigo que quiero que tenga esta funcion
+    document.write('<p>Hola mundo</p>');
+};
+
+//invocar o llamar 
+saludar();
